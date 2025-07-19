0be46b12f7e510382fdae43860201928fdaa2a30
Drishti <adrishti55@gmail.com>
2025-07-19 21:34:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.getElementById('darkModeToggle').addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
